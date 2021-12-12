f1cc811450c31be0418e879c7192a319029d7df1
vanderboon <martin@vanderboon.co.uk>
2021-12-12 17:42:40 +0000

    reordered

--- shell_setup/install.sh
+++ shell_setup/install.sh
@@ -2,11 +2,6 @@
 # Start zsh on startup. No chsh :(
 echo 'bash -c zsh' >> $HOME/.bashrc
 
-# Install Oh-My-Zsh & plugins
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
-#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 # Install fonts
 curl -fsSLO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Mononoki.zip
 unzip Mononoki.zip -d ~/.fonts
@@ -26,3 +21,8 @@ curl -fsSLO https://releases.hashicorp.com/terraform/1.1.0/terraform_1.1.0_linux
 unzip terraform_1.1.0_linux_amd64.zip
 rm terraform_1.1.0_linux_amd64.zip
 mv terraform ~/bin && chmod 0755 ~/bin/terraform 
+
+# Install Oh-My-Zsh & plugins
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
+#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
