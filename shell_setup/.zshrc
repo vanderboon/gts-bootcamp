853763696cc925d26badb7ae09fc25c7c4c6033d
vanderboon <martin@vanderboon.co.uk>
2021-12-12 17:58:16 +0000

    Updates

--- shell_setup/.zshrc
+++ shell_setup/.zshrc
@@ -1,34 +1,11 @@
-export ZSH="$HOME/.oh-my-zsh"
+ZSH="/home/harshith/.oh-my-zsh"
 
-ENABLE_CORRECTION="true"
-COMPLETION_WAITING_DOTS="true"
+#Theme
+ZSH_THEME=""
 
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-plugins=(
-    colorize
-    docker
-    git
-    history
-    terraform
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-)
-
+#Plugins
+plugins=(git zsh-autosuggestions)
 source $ZSH/oh-my-zsh.sh
 
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-export CLICOLOR=1
-export LSCOLORS=exfxcxdxbxegedabagacad
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-
-alias ls="ls -lash"
-
-#Autocomplete
-autoload -U compinit && compinit
-autoload -U +X bashcompinit && bashcompinit
-
-#Init starship
+#Star Ship
 eval "$(starship init zsh)"
--- shell_setup/install.sh
+++ shell_setup/install.sh
@@ -2,6 +2,14 @@
 # Start zsh on startup. No chsh :(
 echo 'bash -c zsh' >> $HOME/.bashrc
 
+# Install Oh-My-Zsh & plugins
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -- --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install starship
+mkdir $HOME/bin
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes --bin-dir $HOME/bin 
+
 # Install fonts
 curl -fsSLO https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Mononoki.zip
 unzip Mononoki.zip -d ~/.fonts
@@ -9,20 +17,14 @@ rm -f Mononoki.zip
 sudo yum -y install fontconfig
 fc-cache -f
 
-# Install starship
-mkdir $HOME/bin
-sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes --bin-dir $HOME/bin 
-
-# Copy standard zsh & powerline configs
-cp ~/gts-bootcamp/shell_setup/.zshrc ~/.zshrc
-
 # Add terraform to bin
 curl -fsSLO https://releases.hashicorp.com/terraform/1.1.0/terraform_1.1.0_linux_amd64.zip
 unzip terraform_1.1.0_linux_amd64.zip
 rm terraform_1.1.0_linux_amd64.zip
 mv terraform ~/bin && chmod 0755 ~/bin/terraform 
 
-# Install Oh-My-Zsh & plugins
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+# Copy standard zsh & powerline configs
+cp ~/gts-bootcamp/shell_setup/.zshrc ~/.zshrc
+
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
 #git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
